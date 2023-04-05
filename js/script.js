9c366636b0b66363049c98f1621171a2a9480928
Sneha-Shaw-Itobuz <sneha@itobuz.com>
2023-04-05 15:14:00 +0530

    `edit and delete function added`

--- js/script.js
+++ js/script.js
@@ -5,21 +5,22 @@ const detailsOverlay = document.querySelector(".details-overlay");
 const detailsCloseBtn = document.querySelector(".details-close");
 const submitNote = document.querySelector(".add-note");
 const noteContainer = document.querySelector(".notes");
+const modifyBtn = document.querySelector(".modify");
 
 addBtn.addEventListener("click", () => {
   formOverlay.classList.toggle("hidden");
 });
 
-formCloseBtn.addEventListener("click", () => {
+formCloseBtn.addEventListener("click", (e) => {
+  e.preventDefault();
   formOverlay.classList.toggle("hidden");
 });
-
-
 
 const API = "http://localhost:8080/notes";
 
 submitNote.addEventListener("click", (e) => {
   e.preventDefault();
+  console.log(form.isImportant.checked);
   if (
     form.title.value.trim().length > 0 &&
     form.content.value.trim().length > 0
@@ -32,6 +33,7 @@ submitNote.addEventListener("click", (e) => {
       body: JSON.stringify({
         title: form.title.value,
         content: form.content.value,
+        isImportant: form.isImportant.checked,
       }),
     }).then((res) => {
       console.log("Request complete! response:", res);
@@ -47,14 +49,70 @@ const getSingleNote = async (id) => {
   const res = await fetch(`${API}/get-single-note/${id}`);
   const data = await res.json();
   return data;
-}
+};
+
+const deleteNote = async (id) => {
+  await fetch(`${API}/delete`, {
+    method: "DELETE",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+    body: JSON.stringify({
+      id: id,
+    }),
+  }).then((res) => {
+    console.log("Request complete! response:", res);
+
+    window.location.reload();
+  });
+};
+
+const editNote = async (id, titleDiv, contentDiv) => {
+  titleDiv.setAttribute("contentEditable", "true");
+  contentDiv.setAttribute("contentEditable", "true");
+
+  modifyBtn.classList.toggle("hidden");
+
+
+  modifyBtn.addEventListener("click", async () => {
+  
+    await fetch(`${API}/update`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify({
+        id: id,
+        title: titleDiv.textContent,
+        content: contentDiv.textContent,
+      }),
+    }).then((res) => {
+      console.log("Request complete! response:", res);
+
+      window.location.reload();
+    });
+  });
+};
 
 const displayDetails = async (id) => {
   const note = await getSingleNote(id);
+
   detailsOverlay.classList.toggle("hidden");
-  detailsOverlay.querySelector(".title").textContent= note.note.title
-  detailsOverlay.querySelector(".content").textContent= note.note.content
-}
+
+  const titleDiv = detailsOverlay.querySelector(".title");
+  const contentDiv = detailsOverlay.querySelector(".content");
+
+  titleDiv.textContent = note.note.title;
+  contentDiv.textContent = note.note.content;
+
+  detailsOverlay.querySelector(".delete").addEventListener("click", () => {
+    deleteNote(id);
+  });
+
+  detailsOverlay.querySelector(".edit").addEventListener("click", () => {
+    editNote(id, titleDiv, contentDiv);
+  });
+};
 
 const getAllNotes = async () => {
   const res = await fetch(`${API}/get`);
@@ -64,25 +122,23 @@ const getAllNotes = async () => {
 
 const renderNotes = async () => {
   const notes = await getAllNotes();
-  // console.log(notes);
-  notes.notes.forEach((note) => {
-    // console.log(note);
+
+  notes.data.forEach((note) => {
     const noteCard = document.createElement("div");
     noteCard.classList.add("note");
     noteCard.innerHTML = `<h3>${note.title}</h3>`;
     noteContainer.appendChild(noteCard);
 
     noteCard.addEventListener("click", () => {
-
-      displayDetails(note._id)
-    }
-    );
+      displayDetails(note._id);
+    });
   });
 };
 
 detailsCloseBtn.addEventListener("click", () => {
-  console.log("hi");
   detailsOverlay.classList.toggle("hidden");
 });
 
-renderNotes();
+(() => {
+  renderNotes();
+})();
